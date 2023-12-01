197575c4a1ca4c5a48036e32108cdc014905f95b
julian-siebert <mail@julian-siebert.de>
2023-12-01 12:06:16 +0100

    feat: Added sendMessage to Commander

--- src/main/java/com/uroria/base/command/Commander.java
+++ src/main/java/com/uroria/base/command/Commander.java
@@ -1,9 +1,11 @@
 package com.uroria.base.command;
 
 import com.uroria.base.lang.Languageable;
+import net.kyori.adventure.text.Component;
 
 public interface Commander extends Languageable {
 
     boolean hasPermission(String permission);
 
+    void sendMessage(Component component);
 }
