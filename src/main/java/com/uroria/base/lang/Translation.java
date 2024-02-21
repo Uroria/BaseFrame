1fedc524ab8319f2a0394d08088cff914376e32f
Raphael Goetz <raphael.goetz.7@gmail.com>
2024-02-21 14:54:25 +0100

    feat: adjusted Translation object to support multiple TagResolver

--- src/main/java/com/uroria/base/lang/Translation.java
+++ src/main/java/com/uroria/base/lang/Translation.java
@@ -11,63 +11,47 @@ import java.util.Arrays;
 @UtilityClass
 public class Translation {
 
-    public Component component(Language language, String key, TagResolver resolver) {
+    public Component component(Language language, String key, TagResolver... resolver) {
         return ComponentUtils.deserialize(TranslationConfigurations.getTranslation(key, language), resolver);
     }
 
-    public Component component(Language language, String key) {
-        return component(language, key, null);
-    }
-
-    public Component component(Languageable user, String key, TagResolver resolver) {
+    public Component component(Languageable user, String key, TagResolver... resolver) {
         return component(user.getLanguage(), key, resolver);
     }
 
     public Component component(Languageable user, String key) {
-        return component(user.getLanguage(), key, null);
+        return component(user.getLanguage(), key);
     }
 
-    public String string(Language language, String key, TagResolver resolver) {
+    public String string(Language language, String key, TagResolver... resolver) {
         return ComponentUtils.serialize(component(language, key, resolver));
     }
 
-    public String string(Language language, String key) {
-        return string(language, key, null);
-    }
-
-    public String string(Languageable user, String key, TagResolver resolver) {
+    public String string(Languageable user, String key, TagResolver... resolver) {
         return string(user.getLanguage(), key, resolver);
     }
 
     public String string(Languageable user, String key) {
-        return string(user.getLanguage(), key, null);
+        return string(user.getLanguage(), key);
     }
 
-    public Component[] components(Language language, String key, TagResolver resolver) {
+    public Component[] components(Language language, String key, TagResolver... resolver) {
         return TranslationConfigurations.getTranslations(key, language).stream().map(s -> ComponentUtils.deserialize(s, resolver)).toArray(Component[]::new);
     }
 
-    public Component[] components(Language language, String key) {
-        return components(language, key, null);
-    }
-
-    public Component[] components(Languageable user, String key, TagResolver resolver) {
+    public Component[] components(Languageable user, String key, TagResolver... resolver) {
         return components(user.getLanguage(), key, resolver);
     }
 
     public Component[] components(Languageable user, String key) {
-        return components(user.getLanguage(), key, null);
+        return components(user.getLanguage(), key);
     }
 
-    public String[] strings(Language language, String key, TagResolver resolver) {
+    public String[] strings(Language language, String key, TagResolver... resolver) {
         return Arrays.stream(components(language, key, resolver)).map(ComponentUtils::serialize).toArray(String[]::new);
     }
 
-    public String[] strings(Language language, String key) {
-        return strings(language, key, null);
-    }
-
-    public String[] strings(Languageable user, String key, TagResolver resolver) {
+    public String[] strings(Languageable user, String key, TagResolver... resolver) {
         return strings(user.getLanguage(), key, resolver);
     }
 
--- src/main/java/com/uroria/base/utils/ComponentUtils.java
+++ src/main/java/com/uroria/base/utils/ComponentUtils.java
@@ -9,23 +9,21 @@ import org.jetbrains.annotations.Nullable;
 
 @UtilityClass
 public class ComponentUtils {
-    public Component deserialize(@Nullable String input) {
-        return deserialize(input, null);
+
+    public Component deserialize(String input) {
+        return MiniMessage.miniMessage().deserialize(input);
     }
 
-    public Component deserialize(@Nullable String input, @Nullable TagResolver tagResolver) {
+    public Component deserialize(@Nullable String input, TagResolver... tagResolver) {
         if (input == null) return Component.empty();
-        if (tagResolver == null) {
-            return MiniMessage.miniMessage().deserialize(input);
-        }
         return MiniMessage.miniMessage().deserialize(input, tagResolver);
     }
 
-    public String serialize(@Nullable String input) {
-        return serialize(input, null);
+    public String serialize(String input) {
+        return serialize(deserialize(input));
     }
 
-    public String serialize(@Nullable String input, @Nullable TagResolver resolver) {
+    public String serialize(@Nullable String input, TagResolver... resolver) {
         return serialize(deserialize(input, resolver));
     }
 
